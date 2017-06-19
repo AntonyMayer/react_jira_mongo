34ed3e118b56abfb7e06005d880a16688714d8d1
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-06-19 12:06:37 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui rem cumque officia similique tempora at, voluptas nostrum sunt ad cum totam iure quam, odit consectetur expedita, nisi molestias et numquam!
+      </div>
+    );
+  }
+}
+
+export default App;
