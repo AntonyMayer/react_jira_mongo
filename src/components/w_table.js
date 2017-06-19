3a755c6b134ee7644c7fe87661dea04bed909d70
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-06-19 15:15:44 -0500

    experimenting

--- src/components/w_table.js
+++ src/components/w_table.js
@@ -3,13 +3,22 @@ import React, { Component } from 'react';
 import '../scss/w_table.css';
 
 class TablesWidget extends Component {
-  render() {
-    return (
-      <div className="app">
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui rem cumque officia similique tempora at, voluptas nostrum sunt ad cum totam iure quam, odit consectetur expedita, nisi molestias et numquam!
-      </div>
-    );
-  }
+    render() {
+        let test = false;
+        if (test) {
+            return ( 
+                <div className = "app" >
+                  True~ {JSON.stringify(test)}!
+                </div>
+            );
+        } else {
+            return ( 
+                <div className = "app" >
+                  False~ {JSON.stringify(test)}!
+                </div>
+            );
+        }
+    }
 }
 
 export default TablesWidget;
